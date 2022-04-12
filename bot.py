761599a3c15dd1d12e831da1590cab432028cc43
Kinosyan Alexander <kinosyanz@gmail.com>
2022-04-13 01:50:15 +0300

    First attempt

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 #!venv/bin/python
 import logging
+
 from aiogram import Bot, Dispatcher, executor, types
 from config import BOT_TOKEN
+from config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_URL
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.executor import start_webhook
 
 
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
@@ -12,7 +15,34 @@ dp = Dispatcher(bot, storage=storage)
 
 logging.basicConfig(level=logging.INFO)
 
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+    # users.close() ???
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
+
 from commands import *
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    # executor.start_polling(dp, skip_updates=True)
+    start_webhook(dispatcher=dp,
+                  webhook_path=WEBHOOK_PATH,
+                  on_startup=on_startup,
+                  on_shutdown=on_shutdown,
+                  skip_updates=True,
+                  host=WEBAPP_HOST,
+                  port=WEBAPP_PORT)
--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 BOT_TOKEN = "5006778926:AAGdxokBBiE1jbl20jtfaLlWxPGTN3LscGU"
 DATABASE = 'Dictations.db'
 TMP_DATA_FILENAME = 'users_data'
+
+# webhook settings
+WEBHOOK_HOST = 'https://stormy-dawn-44989.herokuapp.com/'
+WEBHOOK_PATH = ''
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 5000
